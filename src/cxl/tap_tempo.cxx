fc2edb5381055d9970bab4e62a0a5f81d59b9883
Benjamin Yates <benjamin@rqdq.com>
2019-03-19 10:32:29 -0500

    TapTempo without heap alloc

--- src/cxl/tap_tempo.cxx
+++ src/cxl/tap_tempo.cxx
@@ -1,5 +1,6 @@
 #include "src/cxl/tap_tempo.hxx"
 
+#include <algorithm>
 #include <numeric>
 #include <stdexcept>
 
@@ -11,7 +12,7 @@
 namespace rqdq {
 namespace {
 
-constexpr int kMaxSamples = 16;
+constexpr double kOneMinuteInSecs{60.0};
 
 
 }  // namespace
@@ -26,25 +27,26 @@ void TapTempo::Tap() {
 		d_first = false;
 		d_timer.Reset(); }
 	else {
-		d_buf.push_back(d_timer.GetElapsed());
-		while (d_buf.size() > kMaxSamples) {
-			d_buf.pop_front(); }
-		d_timer.Reset(); }}
+		int beatsSinceStart = d_tapCnt + 1;
+		d_taps[d_headIdx] = d_timer.GetElapsed() / beatsSinceStart;
+		d_tapCnt = std::clamp(d_tapCnt+1, 0, kMaxTapTempoSamples);
+		d_headIdx = (d_headIdx+1) % kMaxTapTempoSamples; }}
 
 
 double TapTempo::GetTempo() {
-	if (d_buf.empty()) {
+	if (d_taps.empty()) {
 		throw std::runtime_error("GetTempo() called with zero taps"); }
-	const auto sum = std::accumulate(begin(d_buf), end(d_buf), 0.0);
-	const auto avgSecondsPerBeat = sum / d_buf.size();
-	const auto bpm = 60.0 / avgSecondsPerBeat;
-	// Log::GetInstance().info(fmt::sprintf("TapTempo::GetTempo() sum: %.4f, avg: %.4f, bpm: %.4f", sum, avgSecondsPerBeat, bpm));
+	const auto sum = std::accumulate(begin(d_taps), begin(d_taps)+d_tapCnt, 0.0);
+	const auto avgSecsPerBeat = sum / d_tapCnt;
+	const auto bpm = kOneMinuteInSecs / avgSecsPerBeat;
+	// Log::GetInstance().info(fmt::sprintf("TapTempo::GetTempo() sum: %.4f, avg: %.4f, bpm: %.4f", sum, avgSecsPerBeat, bpm));
 	return bpm; }
 
 
 void TapTempo::Reset() {
 	d_first = true;
-	d_buf.clear(); }
+	d_tapCnt = 0;
+	d_headIdx = 0; }
 
 
 }  // namespace cxl
--- src/cxl/tap_tempo.hxx
+++ src/cxl/tap_tempo.hxx
@@ -1,23 +1,31 @@
 #pragma once
 
-#include <deque>
+#include <array>
 
 #include "src/rcl/rcls/rcls_timer.hxx"
 
 namespace rqdq {
 namespace cxl {
 
+constexpr int kMaxTapTempoSamples{16};
+
 class TapTempo {
 public:
 	TapTempo();
+	TapTempo(const TapTempo&) = delete;
+	TapTempo& operator=(const TapTempo&) = delete;
+
 	void Tap();
 	void Reset();
 	double GetTempo();
-	int GetNumSamples() const { return d_buf.size(); }
+	int GetNumSamples() const { return d_taps.size(); }
+
 private:
 	rcls::Timer d_timer;
 	bool d_first{true};
-	std::deque<double> d_buf; };
+	int d_tapCnt{0};
+	int d_headIdx{0};
+	std::array<double, kMaxTapTempoSamples> d_taps{}; };
 
 
 }  // namespace cxl
